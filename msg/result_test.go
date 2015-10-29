dbf23cf6971d779ac474221fbef52762bc9226f2
Brady Love <love.brady@gmail.com>
2015-10-29 07:46:22 -0700

    re-write msg/result tests using ginkgo/gomega

--- msg/result_test.go
+++ msg/result_test.go
@@ -1,110 +1,67 @@
 package msg_test
 
 import (
+	. "github.com/bradylove/mescal/msg"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
 	"bytes"
-	"github.com/bradylove/mescal/msg"
-	"testing"
 )
 
-func TestEncodeResult(t *testing.T) {
-	t.Log("Given I have a get result")
-	{
+var _ = Describe("Result", func() {
+	var buffer *bytes.Buffer
+
+	BeforeEach(func() {
 		var b []byte
-		buf := bytes.NewBuffer(b)
+		buffer = bytes.NewBuffer(b)
+	})
 
-		r := msg.NewResult("12345", msg.StatusSuccess, msg.NewGetResult("foo", "bar", int64(1442537301)))
-		if err := r.Encode(buf); err != nil {
-			t.Errorf("It should be able to encode a GetResult %v", failed)
-		}
-		t.Logf("It should be able to encode a GetResult %v", succeed)
+	It("can encode and decode a get result", func() {
+		expiry := int64(1442537301)
+		r := NewResult("12345", StatusSuccess, NewGetResult("foo", "bar", expiry))
 
-		expectedBytes := []byte{0, 0, 0, 0, 10, 0, 0, 0, 0, 0, 0, 0, 1, 0, 2, 0, 1, 0, 1, 0, 1, 0, 0, 0, 25, 0, 0, 0, 50, 0, 0, 0, 0, 0, 0, 0, 1, 0, 2, 0, 85, 95, 251, 85, 0, 0, 0, 0, 5, 0, 0, 0, 34, 0, 0, 0, 5, 0, 0, 0, 34, 0, 0, 0, 102, 111, 111, 0, 0, 0, 0, 0, 98, 97, 114, 0, 0, 0, 0, 0, 49, 50, 51, 52, 53, 0, 0, 0}
-		actualBytes := buf.Bytes()
+		err := r.Encode(buffer)
+		Expect(err).To(BeNil())
+		Expect(buffer.Bytes()).To(Equal(
+			[]byte{0, 0, 0, 0, 10, 0, 0, 0, 0, 0, 0, 0, 1, 0, 2, 0, 1, 0, 1, 0, 1, 0, 0, 0, 25, 0, 0, 0, 50, 0, 0, 0, 0, 0, 0, 0, 1, 0, 2, 0, 85, 95, 251, 85, 0, 0, 0, 0, 5, 0, 0, 0, 34, 0, 0, 0, 5, 0, 0, 0, 34, 0, 0, 0, 102, 111, 111, 0, 0, 0, 0, 0, 98, 97, 114, 0, 0, 0, 0, 0, 49, 50, 51, 52, 53, 0, 0, 0},
+		))
 
-		slicesMatch := true
-		for i := range expectedBytes {
-			if expectedBytes[i] != actualBytes[i] {
-				slicesMatch = false
-			}
-		}
+		decodedRes, err := DecodeResult(buffer)
+		Expect(err).To(BeNil())
+		Expect(decodedRes.Action).To(Equal(ActionGet))
+		Expect(decodedRes.Id).To(Equal("12345"))
 
-		if slicesMatch {
-			t.Logf("It should have the correct bytes %v", succeed)
-		} else {
-			t.Errorf("It should have the correct bytes %v", failed)
-		}
-	}
-}
+		subResult := decodedRes.SubResult.(GetResult)
+		Expect(subResult.Key).To(Equal("foo"))
+		Expect(subResult.Value).To(Equal("bar"))
+		Expect(subResult.Expiry).To(Equal(expiry))
+	})
 
-func TestDecodeResult(t *testing.T) {
-	t.Log("Given I have an encoded get result")
-	{
-		encodedMsg := []byte{0, 0, 0, 0, 10, 0, 0, 0, 0, 0, 0, 0, 1, 0, 2, 0, 1, 0, 0, 0, 1, 0, 0, 0, 25, 0, 0, 0, 50, 0, 0, 0, 0, 0, 0, 0, 1, 0, 2, 0, 85, 95, 251, 85, 0, 0, 0, 0, 5, 0, 0, 0, 34, 0, 0, 0, 5, 0, 0, 0, 34, 0, 0, 0, 102, 111, 111, 0, 0, 0, 0, 0, 98, 97, 114, 0, 0, 0, 0, 0, 49, 50, 51, 52, 53, 0, 0, 0}
-		buf := bytes.NewBuffer(encodedMsg)
+	It("can encode and decode a handshake result", func() {
+		r := NewResult("12345", StatusSuccess, NewHandshakeResult("123"))
 
-		res, err := msg.DecodeResult(buf)
-		if err != nil {
-			t.Fatalf("\tIt should be able to decode a GetResult: %v %v", err, failed)
-		}
+		err := r.Encode(buffer)
+		Expect(err).To(BeNil())
 
-		t.Logf("\tIt should be able to decode a GetResult %v", succeed)
+		decodedRes, err := DecodeResult(buffer)
+		Expect(err).To(BeNil())
+		Expect(decodedRes.Action).To(Equal(ActionHandshake))
+		Expect(decodedRes.Id).To(Equal("12345"))
 
-		if res.Action == msg.ActionGet {
-			t.Logf("\tIt should be a decoded GetResult %v", succeed)
-		} else {
-			t.Logf("\tIt should be a decoded GetResult %v", failed)
-		}
-	}
-}
+		subResult := decodedRes.SubResult.(HandshakeResult)
+		Expect(subResult.ClientId).To(Equal("123"))
+	})
 
-func TestEncodeAndDecodeHandshakeResult(t *testing.T) {
-	t.Log("Given I have a handshake result")
-	{
-		var b []byte
-		buf := bytes.NewBuffer(b)
+	It("can encode and decode a set result", func() {
+		r := NewResult("12345", StatusSuccess, NewSetResult())
 
-		res := msg.NewResult("12345", msg.StatusSuccess, msg.NewHandshakeResult("123"))
-		if err := res.Encode(buf); err != nil {
-			t.Fatalf("\tIt should be able to encode the handshake result: %v %v", err, failed)
-		}
-		t.Logf("\tIt should be able to encode the handshake result %v", succeed)
+		err := r.Encode(buffer)
+		Expect(err).To(BeNil())
 
-		decodedRes, err := msg.DecodeResult(buf)
-		if err != nil {
-			t.Fatalf("\tIt should be able to decode the handshake result: %v %v", err, failed)
-		}
-		t.Logf("\tIt should be able to decode the handshake result %v", succeed)
-
-		if decodedRes.Action == msg.ActionHandshake {
-			t.Logf("\tIt should be of type ActionHandshake %v", succeed)
-		} else {
-			t.Fatalf("\tIt should be of type ActionHandshake %v", failed)
-		}
-	}
-}
-
-func TestEncodeAndDecodeSetResult(t *testing.T) {
-	t.Log("Given I have a set result")
-	{
-		var b []byte
-		buf := bytes.NewBuffer(b)
-
-		res := msg.NewResult("12345", msg.StatusSuccess, msg.NewSetResult())
-		if err := res.Encode(buf); err != nil {
-			t.Fatalf("\tIt should be able to encode the set result: %v %v", err, failed)
-		}
-		t.Logf("\tIt should be able to encode the set result %v", succeed)
-
-		decodedRes, err := msg.DecodeResult(buf)
-		if err != nil {
-			t.Fatalf("\tIt should be able to decode the set result: %v %v", err, failed)
-		}
-		t.Logf("\tIt should be able to decode the set result %v", succeed)
-
-		if decodedRes.Action == msg.ActionSet {
-			t.Logf("\tIt should be of type ActionSet %v", succeed)
-		} else {
-			t.Fatalf("\tIt should be of type ActionSet %v", failed)
-		}
-	}
-}
+		decodedRes, err := DecodeResult(buffer)
+		Expect(err).To(BeNil())
+		Expect(decodedRes.Action).To(Equal(ActionSet))
+		Expect(decodedRes.Id).To(Equal("12345"))
+	})
+})
