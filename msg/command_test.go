eff5dc72b3da087d5baa80dc567d24d34cceb43f
Brady Love <love.brady@gmail.com>
2015-10-29 02:27:19 -0700

    re-write msg/command tests using ginkgo/gomega

--- msg/command_test.go
+++ msg/command_test.go
@@ -1,113 +1,71 @@
 package msg_test
 
 import (
+	. "github.com/bradylove/mescal/msg"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
 	"bytes"
-	"github.com/bradylove/mescal/msg"
-	"testing"
 	"time"
 )
 
-func TestCommandEncode(t *testing.T) {
-	t.Log("Given I have a get command")
-	{
+var _ = Describe("Command", func() {
+	var buffer *bytes.Buffer
+
+	BeforeEach(func() {
 		var b []byte
-		buf := bytes.NewBuffer(b)
+		buffer = bytes.NewBuffer(b)
+	})
 
-		cmd := msg.NewCommand("12345", msg.NewGetCommand("foo"))
-		if err := cmd.Encode(buf); err != nil {
-			t.Errorf("It should be able to encode a GetCommand %v", failed)
-		}
+	It("encodes a get command", func() {
+		cmd := NewCommand("12345", NewGetCommand("foo"))
+		err := cmd.Encode(buffer)
 
-		t.Logf("It should be able to encode a GetCommand %v", succeed)
+		Expect(err).To(BeNil())
+		Expect(buffer.Bytes()).To(Equal(
+			[]byte{0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 1, 0, 2, 0, 1, 0, 1, 0, 0, 0, 0, 0, 13, 0, 0, 0, 50, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 34, 0, 0, 0, 102, 111, 111, 0, 0, 0, 0, 0, 49, 50, 51, 52, 53, 0, 0, 0},
+		))
+	})
 
-		expectedBytes := []byte{0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 1, 0, 2, 0, 1, 0, 1, 0, 0, 0, 0, 0, 13, 0, 0, 0, 50, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 34, 0, 0, 0, 102, 111, 111, 0, 0, 0, 0, 0, 49, 50, 51, 52, 53, 0, 0, 0}
-		actualBytes := buf.Bytes()
-
-		slicesMatch := true
-		for i := range expectedBytes {
-			if expectedBytes[i] != actualBytes[i] {
-				slicesMatch = false
-			}
-		}
-
-		if slicesMatch {
-			t.Logf("It should have the correct bytes %v", succeed)
-		} else {
-			t.Errorf("It should have the correct bytes %v", failed)
-		}
-	}
-}
-
-func TestDecodeCommand(t *testing.T) {
-	t.Log("Given I have an encoded get command")
-	{
+	It("decodes a get command", func() {
 		encodedMsg := []byte{0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 1, 0, 2, 0, 1, 0, 1, 0, 0, 0, 0, 0, 13, 0, 0, 0, 50, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 34, 0, 0, 0, 102, 111, 111, 0, 0, 0, 0, 0, 49, 50, 51, 52, 53, 0, 0, 0}
 		buf := bytes.NewBuffer(encodedMsg)
 
-		cmd, err := msg.DecodeCommand(buf)
-		if err != nil {
-			t.Fatalf("\tIt should be able to decode a GetCommand %v", failed)
-		}
+		cmd, err := DecodeCommand(buf)
+		Expect(err).To(BeNil())
+		Expect(cmd.Id).To(Equal("12345"))
+		Expect(cmd.Action).To(Equal(ActionGet))
 
-		t.Logf("\tIt should be able to decode a GetCommand %v", succeed)
+		subCmd := cmd.SubCommand.(GetCommand)
+		Expect(subCmd.Key).To(Equal("foo"))
+	})
 
-		if cmd.Action == msg.ActionGet {
-			t.Logf("\tIt should be a decoded GetCommand %v", succeed)
-		} else {
-			t.Logf("\tIt should be a decoded GetCommand %v", failed)
-		}
-	}
-}
+	It("encodes and decodes a handshake command", func() {
+		cmd := NewCommand("12345", NewHandshakeCommand("Go v0.1.1"))
+		err := cmd.Encode(buffer)
 
-func TestEncodeAndDecodeHandshakeCommand(t *testing.T) {
-	t.Log("Given I have a handshake command")
-	{
-		var b []byte
-		buf := bytes.NewBuffer(b)
+		Expect(err).To(BeNil())
 
-		cmd := msg.NewCommand("12345", msg.NewHandshakeCommand("Go v0.1.1"))
-		if err := cmd.Encode(buf); err != nil {
-			t.Fatalf("\tIt should be able to encode the handshake command: %v %v", err, failed)
-		}
-		t.Logf("\tIt should be able to encode the handshake command %v", succeed)
+		decodedCmd, err := DecodeCommand(buffer)
+		Expect(decodedCmd.Id).To(Equal("12345"))
+		Expect(decodedCmd.Action).To(Equal(ActionHandshake))
+	})
 
-		decodedCmd, err := msg.DecodeCommand(buf)
-		if err != nil {
-			t.Fatalf("\tIt should be able to decode the handshake command: %v %v", err, failed)
-		}
-		t.Logf("\tIt should be able to decode the handshake command %v", succeed)
+	It("encodes and decodes a set command", func() {
+		expiry := time.Now().Unix()
+		cmd := NewCommand("12345", NewSetCommand("foo", "bar", expiry))
+		err := cmd.Encode(buffer)
 
-		if decodedCmd.Action == msg.ActionHandshake {
-			t.Logf("\tIt should be of type ActionHandshake %v", succeed)
-		} else {
-			t.Fatalf("\tIt should be of type ActionHandshake %v", failed)
-		}
-	}
-}
+		Expect(err).To(BeNil())
 
-func TestEncodeAndDecodeSetCommand(t *testing.T) {
-	t.Log("Given I have a set command")
-	{
-		var b []byte
-		buf := bytes.NewBuffer(b)
+		decodedCmd, err := DecodeCommand(buffer)
+		Expect(decodedCmd.Id).To(Equal("12345"))
+		Expect(decodedCmd.Action).To(Equal(ActionSet))
 
-		cmd := msg.NewCommand("12345", msg.NewSetCommand("foo", "bar", time.Now().Unix()))
-		if err := cmd.Encode(buf); err != nil {
-			t.Fatalf("\tIt should be able to encode the set command: %v %v", err, failed)
-		}
-
-		t.Logf("\tIt should be able to encode the set command %v", succeed)
-
-		decodedCmd, err := msg.DecodeCommand(buf)
-		if err != nil {
-			t.Fatalf("\tIt should be able to decode the set command: %v %v", err, failed)
-		}
-		t.Logf("\tIt should be able to decode the set command %v", succeed)
-
-		if decodedCmd.Action == msg.ActionSet {
-			t.Logf("\tIt should be of type ActionSet %v", succeed)
-		} else {
-			t.Fatalf("\tIt should be of type ActionSet %v", failed)
-		}
-	}
-}
+		subCmd := cmd.SubCommand.(SetCommand)
+		Expect(subCmd.Key).To(Equal("foo"))
+		Expect(subCmd.Value).To(Equal("bar"))
+		Expect(subCmd.Expiry).To(Equal(expiry))
+	})
+})
