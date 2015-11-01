a39f30f98753c85063c576fe121d17675e2f1fd3
Brady Love <love.brady@gmail.com>
2015-11-01 07:31:33 -0800

    added cli flag for port

--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@
 go get -u -t zombiezen.com/go/capnproto/...
 go get github.com/onsi/ginkgo/ginkgo
 go get github.com/onsi/gomega
+go get gopkg.in/alecthomas/kingpin.v2
 
 ginkgo -r
--- server/config.go
+++ server/config.go
@@ -1,11 +1,19 @@
 package main
 
+import (
+	"gopkg.in/alecthomas/kingpin.v2"
+)
+
 type Config struct {
 	Port string
 }
 
 func NewConfig() Config {
+	port := kingpin.Flag("port", "Port to run the server on.").Short('p').String()
+
+	kingpin.Parse()
+
 	return Config{
-		Port: "8899",
+		Port: *port,
 	}
 }
