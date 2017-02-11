05e827d165e9608775e073087b92fc63d77c6f12
Brady Love <blove@pivotal.io>
2017-02-11 14:30:47 -0700

    Move get-deps.sh to scripts dir

--- scripts/get-deps.sh
+++ scripts/get-deps.sh
@@ -3,3 +3,4 @@ go get github.com/stretchr/testify
 go get github.com/nelsam/hel
 go get github.com/golang/protobuf/{proto,protoc-gen-go}
 go get golang.org/x/net/context
+go get github.com/kyoh86/richgo
