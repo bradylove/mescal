98957a5113c2a11e652715a777bc7d8a4dcbeb73
Brady Love <blove@pivotal.io>
2017-02-11 16:40:48 -0700

    Switch to thread safe store

--- pkg/store/store.go
+++ pkg/store/store.go
@@ -1,21 +1,80 @@
 package store
 
-import "github.com/bradylove/mescal/pkg/mescal"
+import (
+	"errors"
+	"hash/fnv"
+	"sync/atomic"
+	"unsafe"
+
+	"github.com/bradylove/mescal/pkg/mescal"
+)
+
+var (
+	bucketCount     = uint64(104729)
+	sliceCount      = uint64(41)
+	allowableOffset = uint64(5)
+
+	ErrKeyNotFound = errors.New("Key not found")
+)
 
 type Store struct {
-	cache map[string]*mescal.KeyValue
+	cache [][]unsafe.Pointer
 }
 
 func New() *Store {
-	return &Store{cache: make(map[string]*mescal.KeyValue)}
+	s := Store{
+		cache: make([][]unsafe.Pointer, sliceCount),
+	}
+
+	for idx := range s.cache {
+		s.cache[uint64(idx)] = make([]unsafe.Pointer, bucketCount)
+	}
+
+	return &s
 }
 
 func (s Store) Set(kv *mescal.KeyValue) error {
-	s.cache[kv.Key] = kv
+	slIdx, bIdx := buildIndex(kv.Key)
+
+	for i := uint64(0); i < allowableOffset; i++ {
+		newIdx := (bIdx + i) % bucketCount
+
+		old := (*mescal.KeyValue)(atomic.LoadPointer(&s.cache[slIdx][newIdx]))
+
+		if old == nil || old.Key == kv.Key {
+			atomic.SwapPointer(
+				&s.cache[slIdx][newIdx],
+				unsafe.Pointer(kv),
+			)
+
+			return nil
+		}
+	}
 
 	return nil
 }
 
 func (s Store) Get(key string) (*mescal.KeyValue, error) {
-	return s.cache[key], nil
+	slIdx, bIdx := buildIndex(key)
+
+	for i := uint64(0); i < allowableOffset; i++ {
+		newIdx := (bIdx + i) % bucketCount
+
+		kv := (*mescal.KeyValue)(atomic.LoadPointer(&s.cache[slIdx][newIdx]))
+
+		if kv != nil && kv.Key == key {
+			return kv, nil
+		}
+	}
+
+	return nil, ErrKeyNotFound
+}
+
+func buildIndex(key string) (sliceIdx uint64, bucketIdx uint64) {
+	f := fnv.New64a()
+	f.Write([]byte(key))
+
+	sum := f.Sum64()
+
+	return sum % sliceCount, sum % bucketCount
 }
--- pkg/store/store_test.go
+++ pkg/store/store_test.go
@@ -1,6 +1,11 @@
 package store_test
 
-import "github.com/bradylove/mescal/pkg/mescal"
+import (
+	"fmt"
+	"math/rand"
+
+	"github.com/bradylove/mescal/pkg/mescal"
+)
 
 func (s *StoreTestSuite) TestSetAndGet() {
 	inkv := &mescal.KeyValue{
@@ -15,4 +20,59 @@ func (s *StoreTestSuite) TestSetAndGet() {
 	s.Nil(err)
 	s.Equal(outkv.Key, "a-key")
 	s.Equal(outkv.GetText(), "a-value")
+}
+
+func (s *StoreTestSuite) TestGettingAKeyThatDoesNotExist() {
+	_, err := s.store.Get("a-key")
+	s.NotNil(err)
+}
+
+func (s *StoreTestSuite) TestSetAndGetDoNotHaveDataRace() {
+	inkv := &mescal.KeyValue{
+		Key:   "a-key",
+		Value: &mescal.KeyValue_Text{Text: "a-value"},
+	}
+
+	go func() {
+		for i := 0; i < 10000; i++ {
+			s.store.Set(inkv)
+		}
+	}()
+
+	for i := 0; i < 10000; i++ {
+		s.store.Get("a-key")
+	}
+}
+
+func (s *StoreTestSuite) TestMinimizingColisions() {
+	keys := make([]string, 1000000)
+
+	for i := range keys {
+		keys[i] = randString(20)
+	}
+
+	for _, k := range keys {
+		inkv := &mescal.KeyValue{
+			Key:   fmt.Sprint(k),
+			Value: &mescal.KeyValue_Text{Text: "a-value"},
+		}
+
+		s.store.Set(inkv)
+	}
+
+	for i, k := range keys {
+		resp, err := s.store.Get(k)
+		s.Nil(err, fmt.Sprint("failed on attempt #", i))
+		s.Equal(resp.Key, k)
+	}
+}
+
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func randString(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
 }
