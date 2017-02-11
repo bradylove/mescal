31b515a89b5dfc396dbe4d877f78b5b9ecb5b238
Brady Love <blove@pivotal.io>
2017-02-10 17:02:55 -0700

    Remove old mescal to make room for new mescal

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-go get -u -t zombiezen.com/go/capnproto/...
-go get github.com/onsi/ginkgo/ginkgo
-go get github.com/onsi/gomega
-go get gopkg.in/alecthomas/kingpin.v2
-
-ginkgo -r
--- msg/protocol/build_capn.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-capnp compile -ogo *.capnp
